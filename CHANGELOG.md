5db0a846518e9ca93baa7339b4ddabac27ba42b1
Brian West <brian@signalwire.com>
2025-06-20 09:02:14 -0500

    bump verison

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## [0.1.26] - 2025-06-20
+
+- Version bump
+
 ## [0.1.25] - 2025-06-19
 
 - Version bump
--- signalwire_agents/__init__.py
+++ signalwire_agents/__init__.py
@@ -18,7 +18,7 @@ A package for building AI agents using SignalWire's AI and SWML capabilities.
 from .core.logging_config import configure_logging
 configure_logging()
 
-__version__ = "0.1.25"
+__version__ = "0.1.26"
 
 # Import core classes for easier access
 from .core.agent_base import AgentBase
